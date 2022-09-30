983c66e0dcd8b24ba417f6d269f581fa2983b057
Ivan <special-ded@yandex.ru>
2022-09-30 17:27:15 +0300

    ls_4 js>burger menu working

--- html_css/assets/mobile-menu.js
+++ html_css/assets/mobile-menu.js
@@ -1,14 +1,40 @@
-let burgerIcon = document.querySelector("div")
-console.log(burgerIcon)
+let burgerIcon = document.querySelector('#burger__icon')
+let crossIcon = document.querySelector('.burger__icon-cross')
+let mobileMenu = document.querySelector('#mobile__menu')
+let menuItem = document.querySelector('.burger__elem-item')
 
-burgerIcon.addEventListener('click', function showMenu() {
+let menuOpen = false;
+
+function menuToggler() {
+  if (menuOpen) {
+    closeMenu()
+  } else {
+    openMenu()
+  }
+  menuOpen = !menuOpen;
+}
+
+burgerIcon.addEventListener('click', () => menuToggler())
+
+crossIcon.addEventListener('click', () => menuToggler())
+
+menuItem.addEventListener('click', () => closeMenu())
+
+function openMenu() {
+  mobileMenu.classList.add('mobile__menu_active');
   console.log('clicked')
+}
+
+function closeMenu() {
+  mobileMenu.classList.remove('mobile__menu_active');
+  console.log('removed')
+}
+
+
+let globalEvent = document.addEventListener('click', (event) => {
+  eventHandler(event)
 })
 
-
-
-
-// var element = document.getElementById("#id");
-// element.addEventListener('click', function () {
-//   console.log("click");
-// });
+function eventHandler(e) {
+  console.log(e)
+}
