bdd660f31bc3f65dcf054ad8ba600f39a5856684
Ivan <special-ded@yandex.ru>
2022-09-30 14:30:49 +0300

    ls_4 js file added

---
+++ html_css/assets/mobile-menu.js
@@ -0,0 +1,14 @@
+let burgerIcon = document.querySelector("div")
+console.log(burgerIcon)
+
+burgerIcon.addEventListener('click', function showMenu() {
+  console.log('clicked')
+})
+
+
+
+
+// var element = document.getElementById("#id");
+// element.addEventListener('click', function () {
+//   console.log("click");
+// });
