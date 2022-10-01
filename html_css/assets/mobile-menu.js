43dd48e133a87694363be17c081ae9890ad36099
Ivan <special-ded@yandex.ru>
2022-10-01 17:46:15 +0300

    ls_4 burger menu done

--- html_css/assets/mobile-menu.js
+++ html_css/assets/mobile-menu.js
@@ -1,8 +1,7 @@
 let burgerIcon = document.querySelector('#burger__icon')
 let crossIcon = document.querySelector('.burger__icon-cross')
 let mobileMenu = document.querySelector('#mobile__menu')
-let menuItem = document.querySelector('.burger__elem-item')
-
+let menuItem = document.querySelectorAll('.burger__elem-item')
 let menuOpen = false;
 
 function menuToggler() {
@@ -18,7 +17,17 @@ burgerIcon.addEventListener('click', () => menuToggler())
 
 crossIcon.addEventListener('click', () => menuToggler())
 
-menuItem.addEventListener('click', () => closeMenu())
+menuItem.forEach(elem => elem.addEventListener('click', () => menuToggler()))
+
+console.log(menuItem)
+
+// closes Mobile menu if width more then 600 px
+
+window.addEventListener('resize', function () {
+  if (window.innerWidth > 599 && menuOpen === true) {
+    menuToggler()
+  }
+})
 
 function openMenu() {
   mobileMenu.classList.add('mobile__menu_active');
@@ -31,10 +40,10 @@ function closeMenu() {
 }
 
 
-let globalEvent = document.addEventListener('click', (event) => {
-  eventHandler(event)
-})
+// let globalEvent = document.addEventListener('click', (event) => {
+//   eventHandler(event)
+// })
 
-function eventHandler(e) {
-  console.log(e)
-}
+// function eventHandler(e) {
+//   console.log(e)
+// }
