bd0eef69610beacf0fc6330d7d56fe873cac2714
Ivan <special-ded@yandex.ru>
2022-10-03 10:25:15 +0300

    ls_4 sticky header done

--- html_css/js/main.js
+++ html_css/js/main.js
@@ -1,3 +1,12 @@
 let header = document.querySelector('.header')
 
-console.log(header)
+console.log(header.clientHeight)
+
+window.addEventListener('scroll', (event) => {
+  console.log(window.scrollY)
+  if (window.scrollY > header.clientHeight) {
+    header.classList.add('sticky');
+  } else {
+    header.classList.remove('sticky')
+  }
+});
