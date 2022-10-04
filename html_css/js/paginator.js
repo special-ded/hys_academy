66977a892edbe46e7c5e8b5c6a9aeb3a81ebb89d
Ivan <special-ded@yandex.ru>
2022-10-04 10:24:54 +0300

    ls_5 dropdown menu done

--- html_css/js/paginator.js
+++ html_css/js/paginator.js
@@ -1,23 +1,39 @@
 let select = function () {
-  let selectHeader = document.querySelector('.input__wrap');
+  let inputField = document.querySelector('.input__wrap');
   let selectItem = document.querySelectorAll('.select__item');
+  let paginatorIconLink = document.getElementsByClassName("paginator__icon-use");
 
-  selectHeader.addEventListener('click', selectToggler)
+  let iconToggle = false
+
+  inputField.addEventListener('click',
+    selectToggler)
 
   selectItem.forEach(item => {
     item.addEventListener('click', selectChoose)
   });
 
   function selectToggler() {
+    iconToggler()
     this.parentElement.classList.toggle('is-active');
   }
 
   function selectChoose() {
+    iconToggler()
     let text = this.innerText,
       select = this.closest('.select'),
       currentText = select.querySelector('.input__value');
     currentText.value = text;
     select.classList.remove('is-active');
+
+  }
+
+  function iconToggler() {
+    if (iconToggle == false) {
+      paginatorIconLink[0].setAttribute('href', "./assets/images/sprite.svg#icon-arrow-active")
+    } else {
+      paginatorIconLink[0].setAttribute('href', "./assets/images/sprite.svg#icon-arrow-default")
+    }
+    iconToggle = !iconToggle
 
   }
 
