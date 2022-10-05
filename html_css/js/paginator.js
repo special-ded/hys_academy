d2e2004521403bc7a77e5949cf6795290065791a
Ivan <special-ded@yandex.ru>
2022-10-05 17:31:34 +0300

    ls_5 slider working=)

--- html_css/js/paginator.js
+++ html_css/js/paginator.js
@@ -1,15 +1,17 @@
 let albumList = document.querySelector('.select__body')
 let sliderScroll = document.querySelector('.blog__slider-scroll')
 let sliderWrapper = document.querySelector('.blog__slider-wrapper')
+let sliderCuantity = sliderWrapper.children.length
 let firstSlide = sliderWrapper.children[0]
 let secondSlide = sliderWrapper.children[1]
 let firstSlideTitle = firstSlide.querySelector('.slider-title')
-firstSlideTitle.innerText = 'wwwwwwwwwwwwww'
-console.log(firstSlideTitle.innerText)
+let secondSlideTitle = secondSlide.querySelector('.slider-title')
+let firstSlideImg = firstSlide.querySelector('.blog__slider-item-img')
+let secondSlideImg = secondSlide.querySelector('.blog__slider-item-img')
 
-let buttonClicked
+let buttonActive = 1
 
-let ImgData = [
+let DATA = [
   {
     "albumId": 1,
     "id": 1,
@@ -45,36 +47,28 @@ let ImgData = [
     "url": "https://via.placeholder.com/600/f66b97",
     "thumbnailUrl": "https://via.placeholder.com/150/f66b97"
   }]
-let albumArr = []
-let liArr = []
-let li = document.createElement('li')
+
+let currentData = []
+
 
 // getting number of button clicked
 sliderScroll.addEventListener('click', (event) => {
-  event.target.className === "blog__slider-btn" || "blog__slider-btn blog__slider-btn_active" ? buttonClicked = event.target.innerHTML : null
-  console.log(buttonClicked)
+  event.target.className === "blog__slider-btn" || "blog__slider-btn blog__slider-btn_active"
+    ? buttonActive = event.target.innerHTML
+    : null
+  getData(sliderCuantity, buttonActive)
 })
 
+function paginator(sliderWrapper, currentData) {
+  currentData.forEach((element, i) => {
+    sliderWrapper.children[i].querySelector('.slider-title').innerText = element.title
+    sliderWrapper.children[i].querySelector('.blog__slider-item-img').src = element.url
+  });
+}
 
-
-function paginator(selector, ImgData) {
-  let arr = []
-  console.log(ImgData[0])
-  ImgData.map((el) => {
-    arr.push(el.albumId)
-  })
-  albumArr = [...new Set(arr)],
-    console.log(albumArr)
-
-
-  for (let i = 0; i < albumArr.length; i++) {
-
-    li.classList.add('select__item');
-    li.innerHTML = `Album ${i}`
-    liArr.push(li)
-    console.log(liArr)
-    buildListAlbums()
-  }
+function getData(cuantity, btn) {
+  console.log(currentData = DATA.slice(cuantity * btn - cuantity, cuantity * btn));
+  paginator(sliderWrapper, currentData)
 }
 
 
