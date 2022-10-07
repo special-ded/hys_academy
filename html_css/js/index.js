baf21d5d9afd99845abdf2d51ea92070739c20b3
Ivan <special-ded@yandex.ru>
2022-10-07 10:55:12 +0300

    ls_5 paginator magor changes

--- html_css/js/index.js
+++ html_css/js/index.js
@@ -1,16 +1,27 @@
-import './paginator.js';
 import './drop-down-menu.js';
 import './mobile-menu.js';
+import paginator from './paginator.js'
+import DATA from './data/paginator-data.js'
+const sliderWrapper = document.querySelector('#paginator')
 
-// paginator(sliderWrapper, currentData)
 
-let header = document.querySelector('.header')
 
-window.addEventListener('scroll', (event) => {
+init()
 
-  if (window.scrollY > header.clientHeight) {
-    header.classList.add('sticky');
-  } else {
-    header.classList.remove('sticky')
-  }
-});
+function init() {
+  paginator(sliderWrapper, DATA)
+  addStickyHeader()
+}
+
+
+// ADDING STICKY HEADER
+function addStickyHeader() {
+  window.addEventListener('scroll', (event) => {
+    const header = document.querySelector('.header')
+    if (window.scrollY > header.clientHeight) {
+      header.classList.add('sticky');
+    } else {
+      header.classList.remove('sticky')
+    }
+  });
+}
--- html_css/js/paginator.js
+++ html_css/js/paginator.js
@@ -1,96 +1,59 @@
 const sliderScroll = document.querySelector('.blog__slider-scroll')
-const sliderWrapper = document.querySelector('.blog__slider-wrapper')
-const sliderQuantity = sliderWrapper.children.length
-let buttonActive = 3
+const sliderWrapper = document.querySelector('#paginator')
 
-const DATA = [
-  {
-    "albumId": 1,
-    "id": 1,
-    "title": "accusamus beatae ad facilis cum similique qui sunt",
-    "url": "https://via.placeholder.com/600/92c952",
-    "thumbnailUrl": "https://via.placeholder.com/150/92c952"
-  },
-  {
-    "albumId": 1,
-    "id": 2,
-    "title": "reprehenderit est deserunt velit ipsam",
-    "url": "https://via.placeholder.com/600/771796",
-    "thumbnailUrl": "https://via.placeholder.com/150/771796"
-  },
-  {
-    "albumId": 1,
-    "id": 3,
-    "title": "officia porro iure quia iusto qui ipsa ut modi",
-    "url": "https://via.placeholder.com/600/24f355",
-    "thumbnailUrl": "https://via.placeholder.com/150/24f355"
-  },
-  {
-    "albumId": 1,
-    "id": 4,
-    "title": "culpa odio esse rerum omnis laboriosam voluptate repudiandae",
-    "url": "https://via.placeholder.com/600/d32776",
-    "thumbnailUrl": "https://via.placeholder.com/150/d32776"
-  },
-  {
-    "albumId": 1,
-    "id": 5,
-    "title": "natus nisi omnis corporis facere molestiae rerum in",
-    "url": "https://via.placeholder.com/600/f66b97",
-    "thumbnailUrl": "https://via.placeholder.com/150/f66b97"
-  }]
 
-let currentData = []
-let activeBtn = sliderScroll.querySelector('.blog__slider-btn_active')
 
-getData(sliderQuantity, buttonActive)
-btnDisabler()
+export default function paginator(sliderWrapper, DATA) {
+  const slidesQuantity = sliderWrapper.children.length
+  // default active Button
+  const buttonClicked = 3
 
-// getting number of button clicked
-sliderScroll.addEventListener('click', (event) => {
-  event.target.className === "blog__slider-btn" || "blog__slider-btn blog__slider-btn_active"
-    ? (buttonActive = event.target.innerHTML, activeBtnToggler(event))
-    : null
-  getData(sliderQuantity, buttonActive)
-})
+  clickHandler(slidesQuantity, DATA)
+  btnDisabler(slidesQuantity, DATA)
+  updateData(slidesQuantity, buttonClicked, DATA)
+}
 
-function paginator(sliderWrapper, currentData) {
+function clickHandler(slidesQuantity, DATA) {
+  let buttonClicked = 3
+  sliderScroll.addEventListener('click', (event) => {
+    event.target.className === "blog__slider-btn" || event.target.className === "blog__slider-btn blog__slider-btn_active" ? (buttonClicked = event.target.innerHTML, activeBtnToggler(event)) : null
+    updateData(slidesQuantity, buttonClicked, DATA)
+  })
+}
+
+function updateData(slidesQuantity, buttonClicked, DATA) {
+  const currentData = DATA.slice(slidesQuantity * buttonClicked - slidesQuantity, slidesQuantity * buttonClicked);
+
+  renderNewSlides(currentData)
+  sliderHandler(currentData);
+}
+
+function renderNewSlides(currentData) {
   currentData.forEach((element, i) => {
-    sliderWrapper.children[i].querySelector('.slider-title').innerText = element.title
-    sliderWrapper.children[i].querySelector('.blog__slider-item-img').src = element.url
+    sliderWrapper.children[i].querySelector('.slider-title').innerText = element.title;
+    sliderWrapper.children[i].querySelector('.blog__slider-item-img').src = element.url;
   });
 }
 
-function getData(quantity, btn) {
-  currentData = DATA.slice(quantity * btn - quantity, quantity * btn)
-  sliderHandler(currentData, btn)
-  paginator(sliderWrapper, currentData)
-}
-
 function activeBtnToggler(e) {
+  let activeBtn = sliderScroll.querySelector('.blog__slider-btn_active')
+
   if (sliderScroll.querySelector('.blog__slider-btn_active') && (e.target.className === "blog__slider-btn" ||
     e.target.className === "blog__slider-btn blog__slider-btn_active")) {
-    activeBtn = sliderScroll.querySelector('.blog__slider-btn_active')
+    activeBtn = sliderScroll.querySelector('.blog__slider-btn_active');
     // removing active class
     activeBtn.classList.remove('blog__slider-btn_active');
     // adding active class
-    e.target.classList.add('blog__slider-btn_active')
+    e.target.classList.add('blog__slider-btn_active');
   }
 }
 
-function sliderHandler(data) {
-  data.length === 1 ?
-    sliderWrapper.children[1].classList.add('display-none') :
-    sliderWrapper.children[1].classList.remove('display-none')
+function sliderHandler(currentData) {
+  currentData.length === 1 ? sliderWrapper.children[1].classList.add('display-none') : sliderWrapper.children[1].classList.remove('display-none')
 }
 
-function btnDisabler() {
-  DATA.length / sliderQuantity <= 3
-    ? (sliderScroll.children[3].setAttribute("disabled", ""),
-      sliderScroll.children[4].setAttribute("disabled", ""))
-    : null
-  DATA.length / sliderQuantity <= 4
-    ? sliderScroll.children[4].setAttribute("disabled", "")
-    : null
+function btnDisabler(slidesQuantity, DATA) {
+  DATA.length / slidesQuantity <= 3 ? (sliderScroll.children[3].setAttribute("disabled", ""), sliderScroll.children[4].setAttribute("disabled", "")) : null
+  DATA.length / slidesQuantity <= 4 ? sliderScroll.children[4].setAttribute("disabled", "") : null
 }
 
