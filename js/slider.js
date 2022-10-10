3df8c317a836ebc1c870ddde4027dffcfbb1bedd
Ivan <special-ded@yandex.ru>
2022-10-10 16:20:50 +0300

    ls_6 slider progress

--- js/slider.js
+++ js/slider.js
@@ -4,39 +4,65 @@ export default class Slider {
     this.selector = selector;
     this.data = data;
   }
-  sliderWrapper = document.querySelector('#slider');
-  slide = document.createElement('div');
+
+  slider = document.querySelector('#slider');
+  sliderInner = document.createElement('div');
+
+  slide
+  leftArrow = document.createElement('button');
+  rightArrow = document.createElement('button');
+
 
   initSlider() {
-    this.addClass()
     this.displayInfo()
     this.processDataforSlides(this.data)
   }
 
 
   processDataforSlides(data) {
-    // removeAllChildNodes(sliderWrapper);
-    console.log(data)
+    this.sliderInner.classList.add('prefer__slider-inner')
+    this.sliderInner.setAttribute("style", `width:${data.length * 218}px`);
+    this.slider.setAttribute("style", `width:${data.length * 218}px`);
+
+
+    this.leftArrow.classList.add('slider__arrow-btn')
+
+    this.leftArrow.innerHTML = `<svg class="slider__arrow" viewBox="0 0 33 32" width="40">
+    <use href="./assets/images/sprite.svg#icon-slide-left"></use>
+    </svg>`
+
+    this.slider.appendChild(this.leftArrow);
+
+    this.slider.appendChild(this.sliderInner)
+
+
     data.forEach((slideData) => {
       this.renderTemplate(slideData);
     });
+
+    this.rightArrow.classList.add('slider__arrow-btn')
+
+
+    this.rightArrow.innerHTML = `<svg class="slider__arrow" viewBox="0 0 33 32" width="40">
+    <use href="./assets/images/sprite.svg#icon-slide-right"></use>
+  </svg>`
+
+    this.slider.appendChild(this.rightArrow);
+
   }
 
   renderTemplate(slideData) {
+    this.slide = document.createElement('div');
     console.log(this.slide);
 
     this.slide.classList.add('prefer__slider-slide');
     this.slide.innerHTML = `<img src=${slideData.url} alt="Graphic Design" width="198" />
-    <div class="bg-position slide-1-bg"></div>
-    <p class="prefer__slider-text">${slideData.title}</p>`
+    <div class="bg-position "></div>
+    <p class="prefer__slider-text">${slideData.title.slice(0, 10)}</p>`
 
-    this.sliderWrapper.appendChild(this.slide);
+    this.sliderInner.appendChild(this.slide);
   }
 
-  addClass() {
-    this.sliderWrapper.classList.add('666666666666')
-    console.log(this.sliderWrapper);
-  }
 
   displayInfo() {
     console.log(this.selector, this.data)
