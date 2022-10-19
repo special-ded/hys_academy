4d0d18f208dd2b83e24047c33921e4eb8cd3c24a
Ivan <special-ded@yandex.ru>
2022-10-19 14:39:54 +0300

    ls_7 css minimizer installed

--- html_css/webpack.config.js
+++ html_css/webpack.config.js
@@ -3,12 +3,7 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: {
-    dropdownmenu: './js/drop-down-menu.js',
     index: './js/index.js',
-    mobilemenu: './js/mobile-menu.js',
-    paginator: './js/paginator.js',
-    slider: './js/slider.js',
-    myslick: './js/libs/my-slick.js'
   },
   module: {
     rules: [
