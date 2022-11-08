895213ab4c604f0f0ab1342ecee857359b27720a
Ivan Draganov <draganov.ivan.an@gmail.com>
2022-11-08 16:29:03 +0200

    ls_9 regexp changed

--- html_css/js/storage.js
+++ html_css/js/storage.js
@@ -1,7 +1,8 @@
 export default class Storage {
+
   regName = /^[a-zA-Z]+ [a-zA-Z]+$/;
   regNumber = /\+38+\d{10}$/;
-  regEmail = /^\S+@\S+\.\S+$/;
+  regEmail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
   constructor() {
     this.localStorageSliderData = 'localStorageSliderData';
