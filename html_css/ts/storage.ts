c963f69a887f430a6b66d4881bca1816be3a116f
Ivan Draganov <draganov.ivan.an@gmail.com>
2022-11-18 14:26:07 +0200

    ls_12 webpack server changes

--- html_css/ts/storage.ts
+++ html_css/ts/storage.ts
@@ -13,9 +13,7 @@ export default class Storage {
   regEmail: RegExp = /(?:[a-z0-9_-]+(?:\.[a-z0-9_-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
   constructor() {
-
   }
-
 
   getSliderData(): Array<Data> {
     this.initStorage();
@@ -42,15 +40,16 @@ export default class Storage {
   renderFormData(): void {
     (document.querySelector('#username') as HTMLInputElement).value = this.localStorageUserName
       ? this.localStorageUserName
-      : null;
+      : '';
 
     (document.querySelector('#telephone') as HTMLInputElement).value = this.localStorageTelephone
       ? this.localStorageTelephone
-      : null;
+      : '';
+    console.log((document.querySelector('#telephone') as HTMLInputElement).value);
 
     (document.querySelector('#email') as HTMLInputElement).value = this.localStorageEmail
       ? this.localStorageEmail
-      : null;
+      : '';
   }
 
   setFormToLocalStorage(): void {
